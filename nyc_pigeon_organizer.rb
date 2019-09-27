99cfc3186f6fb59f22295c27e7d4f82cc701e004
Hope Giometti <hopegiometti@gmail.com>
2019-09-27 22:52:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,62 @@
+#def nyc_pigeon_organizer(data)
+#  pigeon_list = {}
+#  data.each do |attribute_name, attributes|
+#    attributes.each do |attribute_value, pigeon_names|
+#      pigeon_names.each do |name|
+#        if !pigeon_list[name]
+#          pigeon_list[name] = {}
+#        end
+#
+#        if !pigeon_list[name][attribute_name]
+#          pigeon_list[name][attribute_name] = []
+#        end
+#
+#        if !pigeon_list[name][attribute_name].include?(attribute_value)
+#          pigeon_list[name][attribute_name] << attribute_value.to_s
+#        end
+#      end
+#    end
+#  end
+#  pigeon_list  
+#end
+
+
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each do |attribute_name, attributes|
-    attributes.each do |attribute_value, pigeon_names|
+  data.each do |trait_type, traits|
+    traits.each do |trait_values, pigeon_names|
       pigeon_names.each do |name|
-        if !pigeon_list[name]
-          pigeon_list[name] = {}
+        if pigeon_list[name] == nil
+          pigeon_list[name] {}
         end
-
-        if !pigeon_list[name][attribute_name]
-          pigeon_list[name][attribute_name] = []
+        
+        if pigeon_list[name][trait_type] == nil 
+          pigeon_list[name][trait_type] = []
         end
-
-        if !pigeon_list[name][attribute_name].include?(attribute_value)
-          pigeon_list[name][attribute_name] << attribute_value.to_s
+        
+        if pigeon_list[name][trait_type].include?(trait_values)
+          pigeon_list[name][trait_type] << trait_values.to_s  
         end
       end
     end
   end
-  pigeon_list  
+  pigeon_list
 end
+#        if !pigeon_list[name]
+#          pigeon_list[name] = {}
+#        end
+#
+#        if !pigeon_list[name][attribute_name]
+#          pigeon_list[name][attribute_name] = []
+#        end
+#
+#        if !pigeon_list[name][attribute_name].include?(attribute_value)
+#          pigeon_list[name][attribute_name] << attribute_value.to_s
+#        end
+#      end
+#    end
+#  end
+#  pigeon_list  
+#end
+
+
